d7dfb3cada4d20e04499171937595344664e5227
viki-new-tech <vickypurush96@gmail.com>
2025-04-18 19:24:15 +0530

    Project

---
+++ src/com/omrbranch/Gopal.java
@@ -0,0 +1,24 @@
+package com.omrbranch;
+
+public class Gopal {
+
+	
+	public void method1()
+	{
+		System.out.println("Gopal class");
+		
+	}
+	public void method2() {
+		
+		System.out.println("vignesh software tester");
+	}
+	
+	public static void main(String[] args) {
+		
+		Gopal gopal=new Gopal();
+		gopal.method1();
+		gopal.method2();
+		
+		
+	}
+}
