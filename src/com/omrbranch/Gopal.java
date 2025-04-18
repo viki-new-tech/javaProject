5280172d8511c8255c180a8689ffff3a896b58b9
viki-new-tech <vickypurush96@gmail.com>
2025-04-18 20:09:53 +0530

    Update Gopal.java

--- src/com/omrbranch/Gopal.java
+++ src/com/omrbranch/Gopal.java
@@ -5,7 +5,7 @@ public class Gopal {
 	
 	public void method1()
 	{
-		System.out.println("Vignesh class");
+		System.out.println("Vignesh and rajesh and arvind and ramesh");
 		
 	}
 	public void method2() {
